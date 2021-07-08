72d294e49de0cb5511ac47a285196f66a080585f
Matheus Pereira <matheus.tecinfor98@gmail.com>
2021-07-07 22:25:29 -0300

    feat: iniciando com express

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import 'reflect-metadata';
+import express from 'express';
+import routes from './routes/routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3000, () => console.log(' Server started at http://localhost:3000'));
